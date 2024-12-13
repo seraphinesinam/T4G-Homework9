9765b7ae1e3ac750a034bfdfc4095d9077f93f3b
unknown <esinamseraphine7@gmail>
2024-12-13 05:52:29 -0800

    Adding all homework9 files

---
+++ src/index.js
@@ -0,0 +1,49 @@
+// alert("hi");
+// console.log("hi");
+let numberone = document.getElementById('num1');
+numberone.style.backgroundColor="grey"
+numberone.innerHTML="i know now what is the DOM is🙌";
+numberone.style.color="black"
+
+let numbertwo = document.getElementById("num2");
+numbertwo.style.backgroundColor="yellow"
+numbertwo.innerHTML="i know how to access/select elements in the DOM🙏";
+numbertwo.style.color="black"
+
+let numberthree=document.getElementById("num3");
+numberthree.style.backgroundColor="green"
+numberthree.innerHTML="i know how to use the style methods to change the style of an element in the DOM✅"
+numberthree.style.color="black"
+
+let numberfour=document.getElementById("num4");
+numberfour.style.backgroundColor="purpel"
+numberfour.innerHTML="i know how the styling properties differ in js from css"
+numberfour.style.color="white"
+
+let numberfive=document.getElementById("num5");
+numberfive.style.backgroundColor="black"
+numberfive.innerHTML="yes idid it!! I manipulated the DOM and styled my elements 😍😍😍"
+numberfive.style.color="white"
+// let num3=document.querySelector(".num3");
+// num3.style.backgroundColor="green"
+// num3.innerHTML="i know how to use the style methods to change the style of an element in the DOM✅"
+// num3.style.color="black"
+
+// let num4=document.querySelector(".num4");
+// num4.style. backgroundColor="purple"
+// num4.innerHTML="i know how the styling properties differ in jsfrom css👍"
+// num4.style.color="white"
+
+// let num5=document.querySelector(".num5");
+// num5.style.backgroundColor="black"
+// num5.innerHTML="yes i did it!! I manipulated the DOMand styled my elements😘😘"
+// num5.style.color="white"
+
+// let num5=document.querySelector(".num5");
+// num5.style.innerHTML="Yes i did it!! i manipulated the DOM and styled my elements😍😍😍"
+// num5.style.color="white"
+
+
+
+
+
